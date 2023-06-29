e12357054361c71874dcff18d37195696f1c01ba
rishabhagrawalzra <rishabhagrawalzra@gmail.com>
2023-06-29 14:45:00 +0530

    1:collecting arguments and reading file content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_name: &String = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file - {}", file_name);
+
+    let content =
+        fs::read_to_string(file_name).expect("Unable to read and covert into string from file");
+
+    println!("With text: \n{}", content);
 }
