a0623cf801a8f0b8ee000530461ecde9935d7850
rishabhagrawalzra <rishabhagrawalzra@gmail.com>
2023-06-29 16:19:11 +0530

    2:extract the arg parser and group config values

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,26 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_name: &String = &args[2];
+    let config = parse_config(&args);
+    println!("{}", args[1]);
+
+    let content = fs::read_to_string(config.file_name)
+        .expect("Unable to read and covert into string from file");
+
+    println!("With text: \n{}", content);
+}
+
+struct Config {
+    query: String,
+    file_name: String,
+}
+
+fn parse_config(args: &Vec<String>) -> Config {
+    let query = args[1].clone();
+    let file_name = args[2].clone();
 
     println!("Searching for {}", query);
     println!("In file - {}", file_name);
 
-    let content =
-        fs::read_to_string(file_name).expect("Unable to read and covert into string from file");
-
-    println!("With text: \n{}", content);
+    Config { query, file_name }
 }
