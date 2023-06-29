d30636a1fb43f4dcb6510bb8743f51218e10095a
rishabhagrawalzra <rishabhagrawalzra@gmail.com>
2023-06-29 21:33:51 +0530

    6:Splitting code into a library crate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+use mini_grep_simpleCLI_rust::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -13,34 +12,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file - {}", config.file_name);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = mini_grep_simpleCLI_rust::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_name: String,
-}
-
-impl Config {
-    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_name = args[2].clone();
-
-        Ok(Config { query, file_name })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.file_name)?;
-
-    println!("With text: \n{}", content);
-
-    Ok(())
 }
